ee3d86fd686e890c9b4523e981a3b20494eb5218
Jefferson David Rojas Novoa <fejerson108@gmail.com>
2017-06-02 15:24:10 -0500

    Add VCS

---
+++ sample/src/main/java/com/github/jeffersonrojas/materialpermissions/sample/SampleActivity.kt
@@ -0,0 +1,12 @@
+package com.github.jeffersonrojas.materialpermissions.sample
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class SampleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sample)
+    }
+}
